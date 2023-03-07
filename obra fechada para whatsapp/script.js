7798171a11910628531725c3275f49c205a05b9a
wallacedevsantos <wallacedev.oliveirasantos@gmail.com>
2023-03-07 14:59:12 -0300

    adição de campo no gerador de obra fechada

--- obra fechada para whatsapp/script.js
+++ obra fechada para whatsapp/script.js
@@ -4,6 +4,7 @@ function gerarTexto() {
   var nomeObra = document.getElementById("nomeObra").value;
   var sistema = document.getElementById("sistema").value;
   var data = document.getElementById("data").value;
+  var obs = document.getElementById("obs").value;
 
   var texto = document.getElementById("carta-conteudo");
 
@@ -12,6 +13,7 @@ function gerarTexto() {
   texto.innerHTML = texto.innerHTML.replace("{CONSTRUTORA}", construtora);
   texto.innerHTML = texto.innerHTML.replace("{SISTEMA}", sistema);
   texto.innerHTML = texto.innerHTML.replace("{DATA}", data);
+  texto.innerHTML = texto.innerHTML.replace("{OBS}", obs);
 
   var form = document.getElementById("formulario");
   var botao = document.getElementById("botaoGerarTexto");
